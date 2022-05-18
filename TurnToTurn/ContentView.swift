2df235013ae231bce53366eed3582fb5d3b52311
Nabil_saadi <nabil.saadi@codersdc.com>
2022-05-18 14:13:40 +0300

    Initial Commit

---
+++ TurnToTurn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TurnToTurn
+//
+//  Created by Yousef on 5/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TurnToTurn/TurnToTurnApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurnToTurnApp.swift
+//  TurnToTurn
+//
+//  Created by Yousef on 5/18/22.
+//
+
+import SwiftUI
+
+@main
+struct TurnToTurnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
